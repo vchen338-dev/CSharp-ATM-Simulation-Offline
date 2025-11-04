1f00b34d8f7aa48895034e433914686285413399
vchen338-dev <vchen338@gmail.com>
2025-11-04 19:18:52 +0800

    test

--- BankData.cs
+++ BankData.cs
@@ -9,7 +9,7 @@ namespace ATM_Simulation__Offline_
     internal class BankData
     {
         public static string Name = "Test User";
-        public static string Pin = "12345";
+        public static string Pin = "1234";
         public static double Balance = 0;
 
     }
